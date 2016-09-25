9bf76f50a9f5373d5295cdc53f9a83dcc956be84
mars-yc <250085970@qq.com>
2016-09-26 02:33:37 +0800

    .

--- hibernate/src/main/java/com/perfume/tech/hibernate/utils/HibernateUtils.java
+++ hibernate/src/main/java/com/perfume/tech/hibernate/utils/HibernateUtils.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+@SuppressWarnings("deprecation")
 public class HibernateUtils {
 	
 	private static final Logger logger = Logger.getLogger(HibernateUtils.class);
